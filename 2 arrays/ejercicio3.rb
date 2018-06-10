6ff0eda4c40b3d1c906b4f6c7a09d51925bf2ee9
Alejandro Olmos <alejandro.olmos@readinessit.com>
2018-06-09 22:18:04 -0400

    Array exercises all done!

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -1,40 +1,38 @@
- # Dado el array:
+# Dado el array:
 
  a = [1,2,3,9,1,4,5,2,3,6,6]
  
- # 1. Crear un método para eliminar todos los números pares del arreglo.
+# 1. Crear un método para eliminar todos los números pares del arreglo.
+def deleteEvenElements(array)
+    arr = array.uniq
+    arr.each do |i|
+        arr.delete_at(i) if array[i] % 2 == 0
+    end
+end
 
-#def deleteEvenElements(array)
- #   arr = array.uniq
-  #  arr.each do |i|
-   #     arr.delete_at(i) if array[i] % 2 == 0
-    #end
-#end
-
-#puts deleteEvenElements(a)
+puts deleteEvenElements(a)
 
 # 2. Crear un método para obtener la suma de todos los elementos del arreglo Utilizando .each
+def sumElements(array)
+    sum = 0  
+    array.each do |i| 
+        sum += i
+    end
+    puts sum
+end
 
-#def sumElements(array)
-    #sum = 0  
-   # array.each do |i| 
-   #     sum += i
-  #  end
- #   puts sum
-#end
-
-#sumElements(a)
+sumElements(a)
 
 # 3. Crear un método para obtener el promedio de un arreglo.
-#def promArray(array)
- #   sum = 0  
-  #  array.each do |i| 
-   #     sum += i
-    #end
-    #(sum.to_f / array.count)
-#end
+def promArray(array)
+    sum = 0  
+    array.each do |i| 
+        sum += i
+    end
+    (sum.to_f / array.count)
+end
 
-#puts promArray(a)
+puts promArray(a)
 
 # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
 def upToArray(array)
--- 3 strings/ejercicio3.rb
+++ 3 strings/ejercicio3.rb
@@ -3,22 +3,22 @@ a = [1, 2, 3, 9, 12, 31, 'domingo']
 b = %w[lunes martes miércoles jueves viernes sábado domingo]
 
 # 1. La concatenación de a y b. (hint: Los elementos que se repiten en a y b pueden aparecer dos veces en el resultado).
-#a.zip(b).each do |i, j|
- #   puts "#{i} #{j}"
-#end
+a.zip(b).each do |i, j|
+    puts "#{i} #{j}"
+end
 
 # 2. La unión de a y b. (hint: Los elementos que se repiten en a y b NO deben aparecer repetidos en el resultado).
-#a.zip(b).each do |i, j|
- #   puts "#{i} #{j}" if i != j
-#end
+a.zip(b).each do |i, j|
+    puts "#{i} #{j}" if i != j
+end
 
 # 3. La intersección de a y b. (hint: El resultado debe estar compuesto por los elementos que se repiten en a y b).
-#a.zip(b).each do |i, j|
- #   puts "#{i} #{j}" if i == j
-#end
+a.zip(b).each do |i, j|
+    puts "#{i} #{j}" if i == j
+end
 
 # 4. Intercalar los elementos: [[1, "lunes"], [2, "martes"], [3, "miércoles"], [9, "jueves"], [12, "viernes"], [31, "sábado"], ["domingo", "domingo"]]
-
+a.zip(b).flatten
 
 
 
